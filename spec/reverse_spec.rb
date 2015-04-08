c2936b5bc8b3f0d87b2cb14df225a4000f234af2
Marlena Compton <marlena.compton@gmail.com>
2015-04-08 10:34:42 -0700

    reversing a string with TDD

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -8,14 +8,22 @@ describe "Given a string reverser" do
       @subject = StringReverser.new('Simplicity is the glory of expression.')
     end
 
-    it "has the original string" do
+    it "saves the original string into a member" do
       expect(@subject.original_string).to eq('Simplicity is the glory of expression.')
+    end
+
+    it "gets a string back from reverse" do
+      expect(@subject.reverse).to be_a_kind_of(String)
+    end
+
+    it "returns the reverse of the original string" do
+      expect(@subject.reverse).to eq('.noisserpxe fo yrolg eht si yticilpmiS')
     end
   end
 
   describe "when it is given a string of less than 2 characters" do
 
-    it "returns an error message" do
+    it "raises an error" do
       expect{StringReverser.new('a')}.to raise_error("The string must have 2 or more characters.")
     end
   end
--- src/reverse.rb
+++ src/reverse.rb
@@ -9,4 +9,17 @@ class StringReverser
     end
   end
 
+
+  def reverse
+    reversed_string = "a" * @original_string.length
+    forwards_iterator = 0
+    backwards_iterator = -1
+
+    reversed_string.each_char do
+    reversed_string[forwards_iterator] = @original_string[backwards_iterator]
+      forwards_iterator += 1
+      backwards_iterator += -1
+    end
+  end
+
 end
